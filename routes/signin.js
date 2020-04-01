72b4df343bdaa3cb0e77a5c5c97d948c33f97a5e
Sagara161994 <sagar161994@gmail.com>
2020-04-02 01:01:36 +0530

    case

--- routes/signin.js
+++ routes/signin.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/user')
+const User = require('../models/User')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { signUpValidation, loginValidation } = require('../Validations/signUp-Validation');
