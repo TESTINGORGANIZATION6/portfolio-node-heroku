16e4187ae811a5766abc3f7aac40279c743fc7be
pprachit09 <pprachit09@gmail.com>
2020-04-19 00:34:24 -0300

    [POR-25] Facebook authentication

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const FacebookStrategy = require('passport-facebook');
 const User = require('../models/User');
 
 passport.use(
@@ -31,6 +32,44 @@ passport.use(
           });
       })
 );
+
+// displayName
+passport.use(
+  new FacebookStrategy({
+    clientID: process.env.clientID_FB,
+    clientSecret: process.env.clientSecret_FB,
+    callbackURL: "/api/auth/facebook/redirect"
+  }, (accessToken,
+      refreshToken,
+      profile,
+      done) => {
+        User.findOne({ sso: profile.id })
+          .then((user,err) => {
+            if(user || err) {
+              console.log('already have user');
+              done(err, user);
+            } else {
+              let email;
+              if(!profile.emails) {
+                email = `${profile.id}@facebook.com`
+              }
+              else {
+                email = profile.emails[0].value
+              }
+              new User({
+                UserName: profile.displayName,
+                sso: profile.id,
+                FirstName: profile.name.givenName,
+                LastName: profile.name.familyName,
+                Email: email,
+                Password: 'assanAhasas@21242323'
+              }).save().then((user, err) => {
+                done(err, user)
+              })
+            }
+          })
+  })
+)
 
 passport.serializeUser((user, done) => {
   done(null, user.id)
